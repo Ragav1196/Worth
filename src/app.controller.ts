503c5335c7ef2b41824acadb0b25734b777ece8b
Ragav1196 <ragavinrap@gmail.com>
2022-07-11 21:53:26 +0530

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,6 +20,7 @@ export class AppController {
   @Post('post')
   @UseInterceptors(FileInterceptor('62c6f2c07034cb001c892456'))
   postFile(@UploadedFile() file) {
+    console.log('Hello');
     console.log(file);
     return file;
   }
